9e6685fb48ddb974f9f71eaae80a72660c3f869b
Manuel Ortega <manuel@enkidu.coop>
2015-09-16 00:57:44 +0200

    Adding translations to Spanish and Esperanto

---
+++ locale/es.js
@@ -0,0 +1,80 @@
+( function() {
+    "use strict";
+
+    window.i18n = {
+
+        /**
+         * Form label
+         *
+         * English: "Your account ID:"
+         */
+        acctID: "ID de tu cuenta:",
+
+        /**
+         * Form placeholder
+         *
+         * Please keep "@example.org" as a domain
+         * since it's a reserved domain name (http://www.iana.org/domains/reserved)
+         *
+         * English: "e.g. user@example.org"
+         */
+        placeholder: "p.e.: usuario@ejemplo.org",
+
+        /**
+         * Heading for the user bio section
+         *
+         * English: "Bio"
+         */
+        bio: "Bio",
+
+        /**
+         * Heading for the group information section
+         *
+         * English: "Description"
+         */
+        description: "Descripción",
+
+        /**
+         * Heading preceding the user's latest notice
+         *
+         * English: "Latest"
+         */
+        latest: "Último mensaje",
+
+        /**
+         * Members - Follows the count of group members
+         *
+         * Example: "42 members"
+         *
+         * English: "members"
+         */
+        members: "miembros",
+
+        /**
+         * Notices - Follows the count of user notices
+         *
+         * Example: "42 notices"
+         *
+         * English: "notices"
+         */
+        notices: "mensajes",
+
+        /**
+         * "Subscribe" link and submit button text
+         *
+         * Also known as "follow"
+         *
+         * English: "Subscribe"
+         */
+        subscribe: "Suscribirme",
+
+        /**
+         * Subscribers - Follows the subscribers count
+         *
+         * Also known as "followers"
+         *
+         * English: "subscribers"
+         */
+        subscribers: "Suscriptores"
+    };
+}() );
