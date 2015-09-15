d7696e0b84f6f4c98e3276eacef94235a4c7f133
Chimo <chimo@chromic.org>
2015-09-15 17:51:52 -0400

    i18n: add French translation

---
+++ locale/fr.js
@@ -0,0 +1,80 @@
+( function() {
+    "use strict";
+
+    window.i18n = {
+
+        /**
+         * Form label
+         *
+         * English: "Your account ID:"
+         */
+        acctID: "Votre compte :",
+
+        /**
+         * Form placeholder
+         *
+         * Please keep "@example.org" as a domain
+         * since it's a reserved domain name (http://www.iana.org/domains/reserved)
+         *
+         * English: "e.g. user@example.org"
+         */
+        placeholder: "ex: utilisateur@example.org",
+
+        /**
+         * Heading for the user bio section
+         *
+         * English: "Bio"
+         */
+        bio: "Bio",
+
+        /**
+         * Heading for the group information section
+         *
+         * English: "Description"
+         */
+        description: "Description",
+
+        /**
+         * Heading preceding the user's latest notice
+         *
+         * English: "Latest"
+         */
+        latest: "Dernière note",
+
+        /**
+         * Members - Follows the count of group members
+         *
+         * Example: "42 members"
+         *
+         * English: "members"
+         */
+        members: "membres",
+
+        /**
+         * Notices - Follows the count of user notices
+         *
+         * Example: "42 notices"
+         *
+         * English: "notices"
+         */
+        notices: "notes",
+
+        /**
+         * "Subscribe" link and submit button text
+         *
+         * Also known as "follow"
+         *
+         * English: "Subscribe"
+         */
+        subscribe: "Suivre",
+
+        /**
+         * Subscribers - Follows the subscribers count
+         *
+         * Also known as "followers"
+         *
+         * English: "subscribers"
+         */
+        subscribers: "suiveurs"
+    };
+}() );
